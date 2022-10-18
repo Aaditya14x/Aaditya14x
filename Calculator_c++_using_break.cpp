4373c83d5156175bbd0a18de3955cfada388736a
Aaditya14x <115134350+Aaditya14x@users.noreply.github.com>
2022-10-18 21:10:49 +0530

    Create Calculator_c++_using_break.cpp

---
+++ Calculator_c++_using_break.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter a number "<<endl;
+    cin>>a;
+    cout<<"Enter a number "<<endl;
+    cin>>b;
+    char op;
+    cout<<"enter the operation you want to perform"<<endl;
+    cin>>op;
+
+    switch(op)
+    {
+        case '+': cout<<(a+b)<<endl;
+                       break;
+        case '-':cout<<(a-b)<<endl;
+                       break;
+        case '*':cout<<(a*b)<<endl;
+                       break;
+        case '/':cout<<(a/b)<<endl;
+                       break;
+        case '%':cout<<(a%b)<<endl;
+                       break;
+        default: cout<<"Please enter a valid operation"<<endl;
+                       break;
+    }
+}
